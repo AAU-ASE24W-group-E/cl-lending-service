2e43816741dbbfc85eb2061dbe251ba8a15b2743
djman <david@jamnig.net>
2025-01-14 18:27:56 +0100

    added get lending by id

--- src/main/java/at/aau/ase/cl/api/LendingResource.java
+++ src/main/java/at/aau/ase/cl/api/LendingResource.java
@@ -1,11 +1,20 @@
 package at.aau.ase.cl.api;
 
 import at.aau.ase.cl.api.model.LendingModel;
+import at.aau.ase.cl.mapper.LendingMapper;
+import at.aau.ase.cl.model.LendingEntity;
 import at.aau.ase.cl.service.LendingService;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
+import java.util.UUID;
 
 @Path("/lendings")
 public class LendingResource {
@@ -18,5 +27,15 @@ public class LendingResource {
         LendingModel createdLending = lendingService.createLending(lendingModel);
 
         return Response.ok(createdLending).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @APIResponse(responseCode = "200", description = "OK", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = LendingModel.class))})
+    public Response getLending(@PathParam("id") UUID id) {
+        LendingEntity lending = lendingService.getLendingById(id);
+        LendingModel model = LendingMapper.INSTANCE.map(lending);
+        return Response.ok(model).build();
     }
 }
--- src/main/java/at/aau/ase/cl/service/LendingService.java
+++ src/main/java/at/aau/ase/cl/service/LendingService.java
@@ -5,9 +5,20 @@ import at.aau.ase.cl.mapper.LendingMapper;
 import at.aau.ase.cl.model.LendingEntity;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
+import org.jboss.logging.Logger;
+
+import java.util.UUID;
 
 @ApplicationScoped
 public class LendingService {
+    private final Logger logger;
+
+    @jakarta.inject.Inject
+    public LendingService(Logger logger) {
+        this.logger = logger;
+    }
+
     @Transactional
     public LendingModel createLending(LendingModel lendingModel) {
         LendingEntity lendingEntity = LendingMapper.INSTANCE.map(lendingModel);
@@ -15,5 +26,17 @@ public class LendingService {
         lendingEntity.persistAndFlush();
 
         return LendingMapper.INSTANCE.map(lendingEntity);
+    }
+
+    public LendingEntity getLendingById(UUID id) {
+        LendingEntity lending = LendingEntity.findById(id);
+
+        if (lending == null) {
+            logger.debugf("Lending with id %s not found", id);
+            throw new NotFoundException("Lending with id " + id + " not found");
+        }
+
+        logger.debugf("Lending with id %s found", id);
+        return lending;
     }
 }
