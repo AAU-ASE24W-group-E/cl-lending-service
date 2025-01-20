b671ffb3834175fd3c2fd521b919456d54f587ae
djamn <david@jamnig.net>
2025-01-20 22:55:15 +0100

    temporarily removed corsfilter to fix issues

--- src/main/java/at/aau/ase/cl/api/interceptor/CorsFilter.java
+++ src/main/java/at/aau/ase/cl/api/interceptor/CorsFilter.java
@@ -10,9 +10,9 @@ public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");       // todo restrict to specific domain in prod later
+/*        responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "http://localhost:5173");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");*/
     }
 }
