afd415047fd7496e08a9f276c490b1ada1ebf9f2
djamn <david@jamnig.net>
2025-01-22 22:04:19 +0100

    fixed db once again

--- src/main/resources/db/migration/V1.0.1__fix_lending_history.sql.sql
+++ src/main/resources/db/migration/V1.0.1__fix_lending_history.sql.sql
@@ -1,19 +1,3 @@
--- TODO need to be updated
-CREATE TABLE lending_requests
-(
-    id               UUID                                               NOT NULL PRIMARY KEY,
-    book_id          UUID                                               NOT NULL,
-    reader_id        UUID                                               NOT NULL,
-    owner_id         UUID                                               NOT NULL,
-    status           VARCHAR(255)                                       NOT NULL,
-    created_at       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at       TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    meeting_time     TIMESTAMP WITH TIME ZONE,
-    meeting_location VARCHAR(255),
-    deadline         TIMESTAMP WITH TIME ZONE
-
-);
-
 CREATE TABLE lending_history
 (
     id                 UUID                                               NOT NULL PRIMARY KEY,
@@ -22,15 +6,3 @@ CREATE TABLE lending_history
     changed_at         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (lending_request_id) REFERENCES lending_requests (id) ON DELETE CASCADE
 );
-
---
--- CREATE TABLE lending_meetings
--- (
---     id                 UUID                                               NOT NULL PRIMARY KEY,
---     lending_request_id UUID                                               NOT NULL,
---     meeting_time       TIMESTAMP WITH TIME ZONE                           NOT NULL,
---     meeting_place      VARCHAR(255)                                       NOT NULL,
---     deadline           TIMESTAMP WITH TIME ZONE,
---     created_at         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
---     FOREIGN KEY (lending_request_id) REFERENCES lending_requests (id) ON DELETE CASCADE
--- );
